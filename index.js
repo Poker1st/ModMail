84bcc88597590a1915dba12291c4224fd058d7c8
Jason Vu <jvu@scu.edu>
2020-11-15 16:55:36 -0800

    Updated from Discord.js v. 11 to v. 12
    Title lol

--- index.js
+++ index.js
@@ -1,44 +1,40 @@
 // Require Packages
 const Discord = require('discord.js');
- 
+const { MessageEmbed } = require('discord.js');
+
 // Configure Packages
 const client = new Discord.Client();
 const prefix = ''; // If you would like to add commands to the bot set this here
-const ownerID = ''; // Set your ID in here. Do it by copy and pasting it through discord.
-const active = new Map();
+const ownerID = ''; // Set your ID in here. Do it by copy and pasting it through discord. 
+const db = require('quick.db');
 
 client.on("error", (e) => console.error(e));
-  client.on("warn", (e) => console.warn(e));
-  client.on("debug", (e) => console.info(e));
-
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
  
 client.on('ready', () => { 
     client.user.setGame('Message me for help!');
-    console.log(`Bot has started, with ${client.users.size} users!`);
+    console.log(`Bot has started, with ${client.users.cache.size} users!`);
+ });
   
-    });
+ client.on("guildCreate", guild => {
+   // This event triggers when the bot joins a guild.
+   console.log(`${guild.name} SERVER JOINED (id: ${guild.id})! KEEP CAUTION!`);
+ });
   
-  client.on("guildCreate", guild => {
-    // This event triggers when the bot joins a guild.
-    console.log(`${guild.name} SERVER JOINED (id: ${guild.id})! KEEP CAUTION!`);
-  });
-  
-  client.on("guildDelete", guild => {
-    // this event triggers when the bot is removed from a guild.
-    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  });
-
-  const db = require('quick.db');
+ client.on("guildDelete", guild => {
+   // this event triggers when the bot is removed from a guild.
+   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+ });
 
 // Listener Events
-client.on('message', async message => { 
-    if (message.author.bot) return;
+client.on('message', async message => {  
     
     // Check if Message is in a DM
     if (message.guild === null) {
         // Fetch Activity Info
         let active = await db.fetch(`support_${message.author.id}`);
-        let guild = client.guilds.get(''); // Your Server ID
+        let guild = client.guilds.cache.get(''); // Your Server ID
         let channel, found = true;
         try {
             if (active) client.channels.get(active.channelID).guild;
@@ -48,9 +44,9 @@ client.on('message', async message => {
         if (!active || !found) {
             // Create Support Channel.
             active = {};
-            let modRoles = guild.roles.find("name", ""); // Find the Mod/Admin roles so only Admin/Mods will see the tickets. Add it in the quotes
-            let everyone = guild.roles.find("name","@" + "everyone");
-            let bot = guild.roles.find("name","Bot");
+            let modRoles = guild.roles.cache.find(r => r.name == ""); // Find the Mod/Admin roles so only Admin/Mods will see the tickets. Add it in the quotes
+            let everyone = guild.roles.cache.find(r => r.name == "@everyone");
+            let bot = guild.roles.cache.find(r => r.name == "Bot");
             channel = await guild.createChannel(`${message.author.username}-${message.author.discriminator}`);
                 channel.setParent(''); // Management Category ID
                 channel.setTopic(`_complete to close the Ticket | ModMail for ${message.author.tag} | ID: ${message.author.id}`);
@@ -67,36 +63,36 @@ client.on('message', async message => {
                     SEND_MESSAGES: true,
                     MANAGE_CHANNELS: true
                 }); // This will set the permissions so only Staff will see the ticket.
-            let author = message.author;
-            const newChannel = new Discord.RichEmbed()
+ 
+            const newChannel = new MessageEmbed()
                 .setColor('36393E')
-                .setAuthor(author.tag, author.displayAvatarURL)
+                .setAuthor(message.author.tag, message.author.displayAvatarURL)
                 .setFooter('ModMail Ticket Created')
-                .addField('User', author)
-                .addField('ID', author.id);
+                .addField('User', message.author)
+                .addField('ID', message.author.id);
             await channel.send(newChannel);
             
-            const newTicket = new Discord.RichEmbed()
+            const newTicket = new MessageEmbed()
                 .setColor('36393E')
-                .setAuthor(`Hello, ${author.tag}`, author.displayAvatarURL)
+                .setAuthor(`Hello, ${message.author.tag}`, message.author.displayAvatarURL)
                 .setFooter('ModMail Ticket Created');
                 
-            await author.send(newTicket);
+            await message.author.send(newTicket);
             
             // Update Active Data
             active.channelID = channel.id;
-            active.targetID = author.id;
+            active.targetID = message.author.id;
         }
         
-        channel = client.channels.get(active.channelID);
-        const dm = new Discord.RichEmbed()
+        channel = client.channels.cache.get(active.channelID);
+        const dm = new MessageEmbed()
             .setColor('36393E')
             .setAuthor(`Thank you, ${message.author.tag}`, message.author.displayAvatarURL)
             .setFooter(`Your message has been sent -- A staff member will be in contact soon.`);
             
         await message.author.send(dm);
         
-        const embed = new Discord.RichEmbed()
+        const embed = new MessageEmbed()
             .setColor('36393E')
             .setAuthor(message.author.tag, message.author.displayAvatarURL)
             .setDescription(message.content)
@@ -111,12 +107,12 @@ client.on('message', async message => {
     let support = await db.fetch(`supportChannel_${message.channel.id}`);
     if (support) {
         support = await db.fetch(`support_${support}`);
-        let supportUser = client.users.get(support.targetID);
+        let supportUser = client.users.cache.get(support.targetID);
         if (!supportUser) return message.channel.delete();
         
         // !complete command
-        if (message.content.toLowerCase() === "_complete") {
-            const complete = new Discord.RichEmbed()
+        if (message.content.toLowerCase() == "${prefix}complete") {
+            const complete = new MessageEmbed()
                 .setColor('36393E')
                 .setAuthor(`Hey, ${supportUser.tag}`, supportUser.displayAvatarURL)
                 .setFooter('Ticket Closed')
@@ -128,13 +124,13 @@ client.on('message', async message => {
                 .catch(console.error);
             return db.delete(`support_${support.targetID}`);
         }
-        const embed = new Discord.RichEmbed()
+        const embed = new MessageEmbed()
             .setColor('36393E')
             .setAuthor(message.author.tag, message.author.displayAvatarURL)
-            .setFooter(`Message Recieved`)
+            .setFooter(`Message Received`)
             .setDescription(message.content);
             
-        client.users.get(support.targetID).send(embed);
+        client.users.cache.get(support.targetID).send(embed);
         message.delete({timeout: 1000});
         embed.setFooter(`Message Sent -- ${supportUser.tag}`).setDescription(message.content);
         return message.channel.send(embed);
@@ -148,7 +144,7 @@ client.on('message', async message => {
   let cmd = args.shift().toLowerCase(); // This variable holds the first item from the args array, which is taken off of the args array and turned into lowercase.
  
   // Return Statements
-  if (!msg.startsWith(prefix)) return; // If the message doesn't start with the prefix, exit the code.
+  if (!msg.startsWith(prefix) || msg.author.bot) return; // If the message doesn't start with the prefix, exit the code.
  
 })
  
